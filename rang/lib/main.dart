bb370ff22de6cfc0e4ec0c580dbb04498a897791
RibalTeeny <46625485+RibalTeeny@users.noreply.github.com>
2020-03-06 15:47:19 +0200

    Update main.dart

--- rang/lib/main.dart
+++ rang/lib/main.dart
@@ -1,7 +1,52 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+import 'package:flutter/material.dart';
 
-import 'package:flutter/widgets.dart';
+import 'Constants.dart';
 
-void main() => runApp(const Center(child: Text('Hello, rang user!', textDirection: TextDirection.ltr)));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[Menu()],
+        ),
+        body: Center(
+          child: Text("Test"),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Menu extends StatefulWidget{
+  Menu():super();
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  String dropdownValue = "Activity Log";
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: Constants.menu.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      })
+          .toList(),
+    );
+  }
+}
