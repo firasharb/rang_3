d1a9a5c15f0c0a5eef8d3d4a7dd6a0e5f9b9714b
Ribal Teeny <rnt09@mail.aub.edu>
2020-03-26 18:01:15 +0200

    Replaced main.dart -- removed constant.dart'

--- rang/lib/main.dart
+++ rang/lib/main.dart
@@ -1,52 +1,140 @@
 import 'package:flutter/material.dart';
 
-import 'Constants.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Widget mainWidget = Home();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: Builder(
+        builder: (context) =>
+        Scaffold(
         appBar: AppBar(
-          actions: <Widget>[Menu()],
         ),
-        body: Center(
-          child: Text("Test"),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              ListTile(
+                title: Text('Home'),
+                onTap: () {
+                  setState(() {
+                    mainWidget = Home();
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Activity Log'),
+                onTap: () {
+                  setState(() {
+                    mainWidget = ActivityLog();
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Live Stream'),
+                onTap: () {
+                  setState(() {
+                    mainWidget = LiveStream();
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Devices'),
+                onTap: () {
+                  setState(() {
+                    mainWidget = Devices();
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Users'),
+                onTap: () {
+                  setState(() {
+                    mainWidget = Users();
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
+        body: mainWidget,
+      ),
       ),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Menu extends StatefulWidget{
-  Menu():super();
-  @override
-  _MenuState createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> {
-  String dropdownValue = "Activity Log";
+class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
-      },
-      items: Constants.menu.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      })
-          .toList(),
+    return Scaffold(
+        body: Center(
+          child: Text("Home Page"),
+        ),
+      );
+  }
+}
+
+class ActivityLog extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Text("Activity Log"),
+        ),
+    );
+  }
+}
+
+class LiveStream extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Text("Live Stream"),
+        ),
+    );
+  }
+}
+
+class Devices extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: Center(
+            child: Text("Devices"),
+          ),
+    );
+  }
+}
+
+class Users extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Text("Users"),
+        ),
     );
   }
 }
