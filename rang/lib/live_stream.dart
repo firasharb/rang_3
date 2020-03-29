5122e45a02f63f92bb6580573d7d4698124b6a66
HusseinSabrawi <44400114+HusseinSabrawi@users.noreply.github.com>
2020-03-29 19:46:15 +0300

    livestream widget updated

--- rang/lib/live_stream.dart
+++ rang/lib/live_stream.dart
@@ -1,14 +1,35 @@
+//the livestream will be displayed on this widget 
+//importing the necessary files 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_vlc_player/vlc_player.dart';
+import 'package:flutter_vlc_player/vlc_player_controller.dart';
+//import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
 class LiveStream extends StatelessWidget {
+  String _streamUrl;
+  VlcPlayerController _vlcViewController = new VlcPlayerController() ;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Live Stream"),
+        
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new VlcPlayer(defaultHeight: 480,
+            defaultWidth: 640, 
+            url: _streamUrl, 
+            controller: _vlcViewController)
+          ]
+           
+          
+       
+       
+        )
       ),
     );
   }
+
 }
+
