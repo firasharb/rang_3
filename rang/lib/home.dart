292eaaf013da5baea2d2804b34202f756bfdc6e1
firasharb <firas-harb@hotmail.com>
2020-03-31 01:06:58 +0300

    .

--- rang/lib/home.dart
+++ rang/lib/home.dart
@@ -1,13 +1,95 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:rang/plugins/firetop/storage/fire_storage_service.dart';
+
+final Color yellow = Color(0xfffbc31b);
+final Color orange = Color(0xfffb6900);
+String image = "images/dog.jpg";
+
 
 class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Home Page"),
+    return MaterialApp(
+      title: 'Firebase Storage Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: LoadFirebaseStorageImage(),
     );
   }
 }
+
+Future<Widget> _getImage(BuildContext context, String image) async {
+  Image m;
+  await FireStorageService.loadFromStorage(context, image).then((downloadUrl) {
+    m = Image.network(
+      downloadUrl.toString(),
+      fit: BoxFit.scaleDown,
+    );
+
+  });
+  return m;
+}
+
+class LoadFirebaseStorageImage extends StatefulWidget {
+  @override
+  _LoadFirbaseStorageImageState createState() =>
+      _LoadFirbaseStorageImageState();
+}
+
+
+
+class _LoadFirbaseStorageImageState extends State<LoadFirebaseStorageImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: FutureBuilder(
+          future: _getImage(context, image),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState ==
+                ConnectionState.done)
+              return Container(
+                height:
+                MediaQuery
+                    .of(context)
+                    .size
+                    .height / 1.25,
+                width:
+                MediaQuery
+                    .of(context)
+                    .size
+                    .width / 1.25,
+                child: snapshot.data,
+              );
+
+            if (snapshot.connectionState ==
+                ConnectionState.waiting)
+              return Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height /
+                      1.25,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width /
+                      1.25,
+                  child: CircularProgressIndicator());
+            return Container();
+          })));}}
+
+
+
+
+
+
+
+
+
+
+
+
